9b70f3f27da8d86b51070b76f54b56dad6900f84
teeman60 <taiye41@gmail.com>
2020-10-18 13:06:11 -0500

    auth and app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < ActionController::API
 
 
-    
+
+    def encode_token(payload)
+        JWT.encode(payload, "softwaredeveloper")
+        
+    end
+
+    def logged_in?
+        
+
+        headers = request.headers["Authorization"]
+        token = headers.split(" ")[1]
+        
+        begin
+            user_id = JWT.decode(token, "softwaredeveloper")[0]["user_id"]
+            @user = User.find(user_id)
+        rescue 
+            @user = nil
+     
+        end
+
+        render json: {error: "Please LogIn"} unless @user
+      
+    end
+
+
+
 end
