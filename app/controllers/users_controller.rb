89c9060ad58b186673d4c48c8fc3d692476e2187
teeman60 <taiye41@gmail.com>
2020-10-24 23:03:37 -0500

    user controller code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,42 @@ class UsersController < ApplicationController
         render json: users 
     end
 
+    def create
+        user = User.new(user_params)
+
+        if user.valid?
+            user.save 
+            render json: {user: UserSerializer.new(user)}, status: :created
+        else
+            render json: {error: "Failed to create a user"}, status: :not_acceptable
+        end
+    end
+
+
+    def show 
+        user = current_user
+        render json: UserSerializer.new(user)
+    end
+    
+
+    def destroy
+        user = current_user
+        user.destroy 
+    end
+
+
+
+    def current_user
+        user = User.find_by(id: params[:id])
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:name, :password, :email)
+    end
+    
+
 
 end
